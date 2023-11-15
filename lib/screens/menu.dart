05321e71c4374f713ba9ab277f93f3057c32d351
kayzaazr <kayza.azura@gmail.com>
2023-11-15 14:51:23 +0700

    Finish Tutorial 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/screens/list_product.dart';
 
 class ShopItem {
   final String name;
@@ -85,6 +87,13 @@ class ShopCard extends StatelessWidget {
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
+          if (item.name == "Tambah Produk") {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const ShopFormPage()));
+          } else if (item.name == "Lihat Produk") {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const ProductPage()));
+      }
         },
         child: Container(
           // Container untuk menyimpan Icon dan Text
